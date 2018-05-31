74529adfa817fd5cf1f8c56a68ddf9326d519a85
Andi Bellstedt <bellstedt@gmx.de>
2018-05-31 13:03:59 +0200

    Initial build
    add module definition files

---
+++ WindowsEventForwarding/WindowsEventForwarding.psm1
@@ -0,0 +1,36 @@
+﻿#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Modul "WindowsEventForwarding"
+# Author:  Andreas Bellstedt
+
+#region type defintion
+#---------------------
+
+#endregion type defintion
+
+
+#region Constants
+#--------------------------
+
+#endregion
+
+
+#region basic functions
+#----------------------
+. $psscriptroot\Functions\Get-WEFSubscription.ps1
+
+#endregion
+
+
+#region Helper functions
+#-----------------------
+
+#endregion
+
+
+Export-ModuleMember -Function *-WEF**
