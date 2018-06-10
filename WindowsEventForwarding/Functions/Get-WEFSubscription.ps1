0ed653598aa21fb2f726487b868b320c6a82c730
Andi Bellstedt <bellstedt@gmx.de>
2018-06-10 16:09:25 +0200

    Bugfix multi computer processing. Add Format-Table output definition

--- WindowsEventForwarding/Functions/Get-WEFSubscription.ps1
+++ WindowsEventForwarding/Functions/Get-WEFSubscription.ps1
@@ -41,7 +41,7 @@ function Get-WEFSubscription {
             ValueFromPipeline = $false,
             ValueFromPipelineByPropertyName = $false)]
         [Alias("host", "hostname", "Computer", "DNSHostName")]
-        [String[]]$ComputerName,
+        [String[]]$ComputerName = $env:COMPUTERNAME,
 
         # For usage with existing PSRemoting session
         [Parameter(Mandatory = $false,
@@ -84,107 +84,117 @@ function Get-WEFSubscription {
     }
 
     Process {
-        # creating session when remoting is used and a session isn't already available
-        if ( $PsCmdlet.ParameterSetName -eq "RemotingWithComputerName" ) {
-            Write-Verbose "Use $($PsCmdlet.ParameterSetName). Creating session to '$($ComputerName)'"
-            $Local:Parameter = @{
-                ComputerName  = $ComputerName
-                Name          = "WEFSession"
-                ErrorAction   = "Stop"
-                ErrorVariable = "SessionError"
-            }
-            if ($Credential) { $Parameter.Add("Credential", $Credential) }
-                $Session = New-PSSession @Parameter
-            Write-Debug "Session '$($Session.Name)' to $($Session.ComputerName) created."
-            Remove-Variable -Name Parameter -Force -Confirm:$false -WhatIf:$false -Debug:$false -Verbose:$false -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
-        }
-
-        # Check service 'Windows Event Collector' - without this, there are not subscriptions possible
-        Write-Debug "Check service 'Windows Event Collector'"
-        if ($Session) {
-            $Service = Invoke-Command -Session $Session -ScriptBlock { Get-Service -Name "wecsvc" } -ErrorAction Stop
-        } else {
-            $Service = Get-Service -Name "wecsvc" -ErrorAction Stop
-        }
-        if ($Service.Status -ne 'Running') {
-            throw "Working with eventlog subscriptions requires  the 'Windows Event Collector' service in running state.  Please ensure that the service is set up correctly or use 'wecutil.exe qc'."
-        }
-        
-        # Get a list of names for all subscriptions available on the system
-        if ($Session) {
-            Write-Debug "Enumerating subscriptions on $($Session.ComputerName)"
-            $SubscriptionEnumeration = Invoke-Command -Session $Session -ScriptBlock { . "$env:windir\system32\wecutil.exe" "enum-subscription" } -ErrorAction Stop
-            Write-Verbose "Found $($SubscriptionEnumeration.count) subscription(s) on $($Session.ComputerName)"
-        } else {
-            Write-debug "Enumerating subscriptions on local sytem"
-            $SubscriptionEnumeration = . "$env:windir\system32\wecutil.exe" "enum-subscription"
-            Write-Verbose "Found $($SubscriptionEnumeration.count) subscription(s) on local sytem"
-        }
-
-        # If parameter name is not specified - query all available subscrptions
-        if (-not $Name) { 
-            Write-Verbose "No name specified. Query all available subscriptions"
-            [array]$Name = $SubscriptionEnumeration 
-        }
-
-        # Looping through every name from parameter, or every subscription found in the system (if parameter was not specified)
-        foreach ($NameItem in $Name) { 
-            # Filtering out the subscriptions to query
-            if($SubscriptionEnumeration.count -gt 1) {
-                $SubscriptionItemsToQuery = $SubscriptionEnumeration -like $NameItem
-            } else {
-                if($SubscriptionEnumeration -like $NameItem) {
-                    [array]$SubscriptionItemsToQuery = $SubscriptionEnumeration
+        Write-Verbose "Parameterset: $($PsCmdlet.ParameterSetName)"
+        foreach ($ComputerNameItem in $ComputerName) {
+            # creating session when remoting is used and a session isn't already available
+            if ( $PsCmdlet.ParameterSetName -eq "RemotingWithComputerName" ) {
+                Write-Verbose "Use $($PsCmdlet.ParameterSetName). Creating session to '$($ComputerNameItem)'"
+                $Local:Parameter = @{
+                    ComputerName  = $ComputerNameItem
+                    Name          = "WEFSession"
+                    ErrorAction   = "Stop"
+                    ErrorVariable = "SessionError"
                 }
+                if ($Credential) { $Parameter.Add("Credential", $Credential) }
+                try {
+                    $Session = New-PSSession @Parameter
+                }
+                catch {
+                    $SessionError | Write-Error 
+                    continue
+                }
+                Write-Debug "Session '$($Session.Name)' to $($Session.ComputerName) created."
+                Remove-Variable -Name Parameter -Force -Confirm:$false -WhatIf:$false -Debug:$false -Verbose:$false -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
+            }
+
+            # Check service 'Windows Event Collector' - without this, there are not subscriptions possible
+            Write-Debug "Check service 'Windows Event Collector'"
+            if ($Session) {
+                $Service = Invoke-Command -Session $Session -ScriptBlock { Get-Service -Name "wecsvc" } -ErrorAction Stop
+            } else {
+                $Service = Get-Service -Name "wecsvc" -ErrorAction Stop
+            }
+            if ($Service.Status -ne 'Running') {
+                throw "Working with eventlog subscriptions requires  the 'Windows Event Collector' service in running state.  Please ensure that the service is set up correctly or use 'wecutil.exe qc'."
             }
             
-            # Query subscription infos if there is a matchin g subscription in the list
-            if ($SubscriptionItemsToQuery) {
-                $Subscriptions = @()
-                foreach ($SubscriptionItemToQuery in $SubscriptionItemsToQuery) {
-                    if ($Session) {
-                        Write-Verbose "Query subscription '$($SubscriptionItemToQuery)' on $($Session.ComputerName)"
-                        [xml]$result = Invoke-Command -Session $Session -ScriptBlock { . "$env:windir\system32\wecutil.exe" "get-subscription" $using:SubscriptionItemToQuery "/format:xml" } -ErrorAction Stop
-                    } else {
-                        Write-Verbose "Query subscription '$($SubscriptionItemToQuery)' on local system"
-                        [xml]$result = . "$env:windir\system32\wecutil.exe" "get-subscription" $SubscriptionItemToQuery "/format:xml"
+            # Get a list of names for all subscriptions available on the system
+            if ($Session) {
+                Write-Debug "Enumerating subscriptions on $($Session.ComputerName)"
+                $SubscriptionEnumeration = Invoke-Command -Session $Session -ScriptBlock { . "$env:windir\system32\wecutil.exe" "enum-subscription" } -ErrorAction Stop
+                Write-Verbose "Found $($SubscriptionEnumeration.count) subscription(s) on $($Session.ComputerName)"
+            } else {
+                Write-debug "Enumerating subscriptions on local sytem"
+                $SubscriptionEnumeration = . "$env:windir\system32\wecutil.exe" "enum-subscription"
+                Write-Verbose "Found $($SubscriptionEnumeration.count) subscription(s) on local sytem"
+            }
+
+            # If parameter name is not specified - query all available subscrptions
+            if (-not $Name) { 
+                Write-Verbose "No name specified. Query all available subscriptions"
+                [array]$Name = $SubscriptionEnumeration 
+            }
+
+            # Looping through every name from parameter, or every subscription found in the system (if parameter was not specified)
+            foreach ($NameItem in $Name) { 
+                # Filtering out the subscriptions to query
+                if($SubscriptionEnumeration.count -gt 1) {
+                    $SubscriptionItemsToQuery = $SubscriptionEnumeration -like $NameItem
+                } else {
+                    if($SubscriptionEnumeration -like $NameItem) {
+                        [array]$SubscriptionItemsToQuery = $SubscriptionEnumeration
                     }
-                    $Subscriptions += $result
-                    Clear-Variable -Name result -Force -Confirm:$false -Verbose:$false
                 }
                 
-            }
-            
-            # Transforming xml infos to powershell objects
-            if (-not $Subscriptions) {
-                Write-Warning "No subscription '$($NameItem)' found on $(if($Session) { $Session.ComputerName } else { "local system"} )"
-            } else {
-                foreach ($Subscription in $Subscriptions) { 
-                    Write-Debug "Working on subscription $($Subscription.Subscription.SubscriptionId)"
-                    
-                    # Compiling the output object
-                    $SubscriptionObjectProperties = [ordered]@{
-                        BaseObject                             = $Subscription
-                        PSSession                              = $Session
+                # Query subscription infos if there is a matchin g subscription in the list
+                if ($SubscriptionItemsToQuery) {
+                    $Subscriptions = @()
+                    foreach ($SubscriptionItemToQuery in $SubscriptionItemsToQuery) {
+                        if ($Session) {
+                            Write-Verbose "Query subscription '$($SubscriptionItemToQuery)' on $($Session.ComputerName)"
+                            [xml]$result = Invoke-Command -Session $Session -ScriptBlock { . "$env:windir\system32\wecutil.exe" "get-subscription" $using:SubscriptionItemToQuery "/format:xml" } -ErrorAction Stop
+                        } else {
+                            Write-Verbose "Query subscription '$($SubscriptionItemToQuery)' on local system"
+                            [xml]$result = . "$env:windir\system32\wecutil.exe" "get-subscription" $SubscriptionItemToQuery "/format:xml"
+                        }
+                        $Subscriptions += $result
+                        Clear-Variable -Name result -Force -Confirm:$false -Verbose:$false
                     }
-                    $Output = New-Object -TypeName psobject -Property $SubscriptionObjectProperties
+                    
+                }
+                
+                # Transforming xml infos to powershell objects
+                if (-not $Subscriptions) {
+                    Write-Warning "No subscription '$($NameItem)' found on $(if($Session) { $Session.ComputerName } else { "local system"} )"
+                } else {
+                    foreach ($Subscription in $Subscriptions) { 
+                        Write-Debug "Working on subscription $($Subscription.Subscription.SubscriptionId)"
+                        
+                        # Compiling the output object
+                        $SubscriptionObjectProperties = [ordered]@{
+                            BaseObject                             = $Subscription
+                            PSSession                              = $Session
+                        }
+                        $Output = New-Object -TypeName psobject -Property $SubscriptionObjectProperties
 
-                    # Add typnames to the output object. this adds all the script properties to the output object,
-                    $Output.pstypenames.Insert(0, $BaseType)
-                    $Output.pstypenames.Insert(0, $TypeName)
-                    $Output.pstypenames.Insert(0, "$($TypeName).$($Subscription.Subscription.SubscriptionType)")
+                        # Add typnames to the output object. this adds all the script properties to the output object,
+                        $Output.pstypenames.Insert(0, $BaseType)
+                        $Output.pstypenames.Insert(0, $TypeName)
+                        $Output.pstypenames.Insert(0, "$($TypeName).$($Subscription.Subscription.SubscriptionType)")
 
-                    #write the object to the pipeline
-                    Write-Output -InputObject $Output
+                        #write the object to the pipeline
+                        Write-Output -InputObject $Output
 
-                    # Clearing up the mess of variables
-                    Remove-Variable -Name SubscriptionObjectProperties -Force -Confirm:$false -WhatIf:$false -Debug:$false -Verbose:$false -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
+                        # Clearing up the mess of variables
+                        Remove-Variable -Name SubscriptionObjectProperties -Force -Confirm:$false -WhatIf:$false -Debug:$false -Verbose:$false -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
+                    }
                 }
             }
-        }
 
-        if ( $PsCmdlet.ParameterSetName -eq "RemotingWithComputerName" ) {
-            $Session | Remove-PSSession -Confirm:$false 
+            if ( $PsCmdlet.ParameterSetName -eq "RemotingWithComputerName" ) {
+                Write-Verbose "Use $($PsCmdlet.ParameterSetName). Closing session to '$($ComputerNameItem)'"
+                $Session | Remove-PSSession -Confirm:$false 
+            }
         }
     }
 
