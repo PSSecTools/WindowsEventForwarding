db1a8ad3d395ad189bdf1aaac252de265a95ec14
Andi Bellstedt <bellstedt@gmx.de>
2018-07-22 16:14:42 +0200

    New function Get-WEFSubscriptionRuntimestatus

---
+++ WindowsEventForwarding/Functions/Get-WEFSubscriptionRuntimestatus.ps1
@@ -0,0 +1,185 @@
+function Get-WEFSubscriptionRuntimestatus {
+    <#
+        .Synopsis
+            Get-WEFSubscriptionRuntimestatus
+
+        .DESCRIPTION
+            Query status information for a Windows Eventlog Forwarding subscription
+
+        .PARAMETER InputObject
+            Pipeline catching object for Get-WEFSubscription
+
+        .PARAMETER ComputerName
+            The computer(s) to connect to.
+            Supports PSSession objects, will reuse sessions.
+
+            Available aliases: "host", "hostname", "Computer", "DNSHostName"
+
+        .PARAMETER Session
+            PSSession(s) to connect to.
+
+        .PARAMETER Name
+            Name of the subscription to query for runtimestatus.
+            Only needed when InputObject is not used.
+            Must be specified when piping in a computername or a session.
+
+            Available aliases: "DisplayName", "SubscriptionID", "Idendity"
+
+        .PARAMETER Credential
+            The credentials to use on remote calls.
+
+        .EXAMPLE
+            PS C:\> Get-WEFSubscriptionRuntimestatus -Name "Subscription1"
+
+            Query status information for subscription "Subscription1"
+
+        .EXAMPLE
+            PS C:\> Get-WEFSubscription -Name "Subscription1" | Get-WEFSubscriptionRuntimestatus
+
+            Query status information for subscription "Subscription1" by using the pipeline.
+
+        .NOTES
+            Author: Andreas Bellstedt
+
+        .LINK
+            https://github.com/AndiBellstedt/WindowsEventForwarding
+    #>
+    [CmdletBinding( DefaultParameterSetName = 'ComputerName',
+        ConfirmImpact = 'low')]
+    Param(
+        [Parameter(ValueFromPipeline = $true, Position = 0, ParameterSetName = "InputObject")]
+        [WEF.Subscription[]]
+        $InputObject,
+
+        [Parameter(ValueFromPipeline = $false, Position = 0, Mandatory = $false, ParameterSetName = "ComputerName")]
+        [Parameter(ValueFromPipeline = $false, Position = 0, Mandatory = $false, ParameterSetName = "Session")]
+        [Alias("DisplayName", "SubscriptionID", "Idendity")]
+        [ValidateNotNullOrEmpty()]
+        [String[]]
+        $Name,
+
+        [Parameter(ValueFromPipeline = $true, Position = 1, Mandatory = $false, ParameterSetName = "ComputerName")]
+        [Alias("host", "hostname", "Computer", "DNSHostName")]
+        [PSFComputer[]]
+        $ComputerName = $env:COMPUTERNAME,
+
+        [Parameter(ParameterSetName = "Session")]
+        [System.Management.Automation.Runspaces.PSSession[]]
+        $Session,
+
+        [PSCredential]
+        $Credential
+    )
+
+    Begin {
+        # If session parameter is used -> transfer it to ComputerName,
+        # The class "PSFComputer" from PSFramework can handle it. This simplifies the handling in the further process block
+        if ($Session) { $ComputerName = $Session }
+    }
+
+    Process {
+        [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+        Write-PSFMessage -Level Debug -Message "ParameterNameSet: $($PsCmdlet.ParameterSetName)"
+
+        #region query specified subscription when not piped in
+        if($PsCmdlet.ParameterSetName -ne "InputObject") {
+            # when not inputobject --> query for existing object to modify
+            Write-PSFMessage -Level Verbose -Message "Gathering $ComputerName for subscription $Name"
+            try {
+                $InputObject = Get-WEFSubscription -Name $Name -ComputerName $ComputerName -ErrorAction Stop
+            } catch {
+                Stop-PSFFunction -Message "Error finding subscription '$name' on computer $computer" -ErrorRecord $_ -EnableException $true
+            }
+            if (-not $InputObject) {
+                $message = "Subscription $Name not found"
+                if($ComputerName) { $message = $message + " on " + $ComputerName }
+                Stop-PSFFunction -Message $message  -ErrorRecord $_ -EnableException $true
+            }
+        }
+        #endregion query specified subscription when not piped in
+
+        foreach ($subscription in $InputObject) {
+            Write-PSFMessage -Level Verbose -Message "Processing '$($subscription.Name)' on '$($subscription.ComputerName)'" -Target $subscription.ComputerName
+
+            #region query status information on subscription from system
+            # Get-Subscriptionruntimestatus. Execute wecutil to get runtimestatus of subscription
+            Write-PSFMessage -Level Verbose -Message "Get runtimestatus for subscription '$($subscription.Name)' on computer '$($subscription.ComputerName)'" -Target $subscription.ComputerName
+
+            $invokeParams = @{
+                ComputerName  = $subscription.ComputerName
+                ErrorAction   = "Stop"
+                ErrorVariable = "ErrorReturn"
+                ArgumentList  = @(
+                    $subscription.Name
+                )
+            }
+            if($Credential) { $invokeParams.Add("Credential", $Credential)}
+
+            try {
+                $invokeOutput = Invoke-PSFCommand @invokeParams -ScriptBlock {
+                    try { [Console]::OutputEncoding = [System.Text.Encoding]::UTF8 } catch {}
+                    $output = . "$env:windir\system32\wecutil.exe" "get-subscriptionruntimestatus" "$($args[0])" *>&1
+                    $wecExceptions = $output | Where-Object { $_.InvocationInfo.MyCommand.Name -like 'wecutil.exe' } *>&1
+                    if($wecExceptions) {
+                        Write-Error -Message "$([string]::Join(" ", $wecExceptions.Exception.Message.Replace("`r`n"," ")))" -ErrorAction Stop
+                    } else {
+                        $output | Where-Object pstypenames -contains 'System.String'
+                    }
+                }
+                if($ErrorReturn) { Write-Error "" -ErrorAction Stop}
+            } catch {
+                $ErrorReturnWEC = $ErrorReturn | Where-Object { $_.InvocationInfo.MyCommand.Name -like 'wecutil.exe' } | select-object -Unique
+                if($ErrorReturnWEC) {
+                    $ErrorMsg = [string]::Join(" ", ($ErrorReturnWEC.Exception.Message.Replace("`r`n"," ") | select-object -Unique))
+                } else {
+                    $ErrorMsg = [string]::Join(" ", ($ErrorReturn.Exception.Message | select-object -Unique))
+                }
+
+                Stop-PSFFunction -Message "Error resuming subscription '$($subscription.Name)' on computer '$($subscription.ComputerName)'! $($ErrorMsg)" -ErrorRecord $_
+            }
+
+            # matching wecutil output and compiling output object
+            if([string]::Join('', $invokeOutput) -match '(^Subscription: (?<SubscriptionId>\S*)\tRunTimeStatus: (?<SubscriptionRuntimeStatus>\S*)\tLastError: (?<SubscriptionLastError>\S*))(\tEventSources:(?<SubscriptionEventSources>.*|\n*$)|(\tErrorMessage: (?<SubscriptionEventErrorMessage>.*|\n*))\tErrorTime: (?<SubscriptionEventsErrorTime>.*|\n*$)|$)') {
+                $SubscriptionRuntimeStatus = $Matches['SubscriptionRuntimeStatus']
+                switch ($SubscriptionRuntimeStatus) {
+                    'Active'   { $keys = "SubscriptionId", "SubscriptionRuntimeStatus", "SubscriptionLastError", "SubscriptionEventSources" }
+                    'Inactive' { $keys = "SubscriptionId", "SubscriptionRuntimeStatus", "SubscriptionLastError", "SubscriptionErrorMessage", "SubscriptionErrorTime" }
+                    'Disabled' { $keys = "SubscriptionId", "SubscriptionRuntimeStatus", "SubscriptionLastError" }
+                    Default { Write-PSFMessage -Level Warning -Message "Unkown runtimestatus found: $($Matches['SubscriptionRuntimeStatus'])" -EnableException $_ }
+                }
+                $hashTableSubscriptionStatus = [ordered]@{}
+                foreach($key in $keys) {
+                    $hashTableSubscriptionStatus.Add($key, $Matches[$key])
+                }
+                if($SubscriptionRuntimeStatus -ne 'Active' ) {
+                    $outputObject = [PSCustomObject]$hashTableSubscriptionStatus
+                    $outputObject.pstypenames.Insert(0,$BaseType)
+                    $outputObject.pstypenames.Insert(0,"$($BaseType).SubscriptionRuntimeStatus")
+                    $outputObject
+                } else {
+                    $matchEventSources = Select-String -InputObject $hashTableSubscriptionStatus['SubscriptionEventSources'] -AllMatches -Pattern '(\t{2}(?<SourceId>\S*)\t{3})(RunTimeStatus: (?<SourceRuntimeStatus>\S*)(\t{3})LastError: )(?<SourceLastError>\S*)(\s{3}LastHeartbeatTime: )(?<SourceLastHeartbeat>\S*)'
+                    foreach($eventSource in $matchEventSources.Matches) {
+                        $hashTableEventSources = [ordered]@{}
+                        foreach($key in ($Keys | Where-Object {$_ -notlike "SubscriptionEventSources"})) {
+                            $hashTableEventSources.Add($key, $hashTableSubscriptionStatus[$key])
+                        }
+                        foreach($EventSourceItem in ($EventSource.groups | Where-Object name -like "source*")) {
+                            $hashTableEventSources.Add($EventSourceItem.Name, $EventSourceItem.Value)
+                        }
+                        $outputObject = [PSCustomObject]$hashTableEventSources
+                        $outputObject.pstypenames.Insert(0,$BaseType)
+                        $outputObject.pstypenames.Insert(0,"$($BaseType).SubscriptionRuntimeStatus")
+                        $outputObject
+                    }
+                }
+            } else {
+                $message = "Warning! Can't retrieve runtimestatus for subscription '$($subscription.Name)' on computer '$($subscription.ComputerName)'"
+                Write-PSFMessage -Level Warning -Message $message -Target $subscription.ComputerName -EnableException $true
+           }
+            #endregion  query status information on subscription from system
+        }
+    }
+
+    End {
+    }
+}
--- WindowsEventForwarding/WindowsEventForwarding.psd1
+++ WindowsEventForwarding/WindowsEventForwarding.psd1
@@ -1,76 +1,77 @@
 @{
 	# Script module or binary module file associated with this manifest
 	ModuleToProcess = 'WindowsEventForwarding.psm1'
-	
+
 	# Version number of this module.
-	ModuleVersion = '0.9.0.0'
-	
+	ModuleVersion = '0.9.1.0'
+
 	# ID used to uniquely identify this module
 	GUID = '5a870944-db6c-4dc2-90f1-738a8c3a07f4'
-	
+
 	# Author of this module
 	Author = 'Andreas Bellstedt'
-	
+
 	# Company or vendor of this module
 	CompanyName = ' '
-	
+
 	# Copyright statement for this module
 	Copyright = 'Copyright (c) 2018 Andreas Bellstedt'
-	
+
 	# Description of the functionality provided by this module
 	Description	     = @'
 Module for handling subscriptions for Windows Event Forwarding plattform.
 
-All cmdlets are build with 
+All cmdlets are build with
 - powershell regular verbs
 - with pipeling availabilties where it is practical
 - logging on verbose channel
 '@
-	
+
 	# Minimum version of the Windows PowerShell engine required by this module
 	PowerShellVersion = '5.0'
-	
+
 	# Name of the Windows PowerShell host required by this module
 	# PowerShellHostName = ''
-	
+
 	# Minimum version of the Windows PowerShell host required by this module
 	# PowerShellHostVersion = ''
-	
+
 	# Minimum version of the .NET Framework required by this module
 	# DotNetFrameworkVersion = '2.0'
-	
+
 	# Minimum version of the common language runtime (CLR) required by this module
 	# CLRVersion = '2.0.50727'
-	
+
 	# Processor architecture (None, X86, Amd64, IA64) required by this module
 	# ProcessorArchitecture = 'None'
-	
+
 	# Modules that must be imported into the global environment prior to importing
 	# this module
 	RequiredModules = @(
 		@{ ModuleName='PSFramework'; ModuleVersion= '0.9.21.61' }
 	)
-	
+
 	# Assemblies that must be loaded prior to importing this module
 	RequiredAssemblies = @( 'bin\WindowsEventForwarding.dll' )
-	
+
 	# Script files (.ps1) that are run in the caller's environment prior to
 	# importing this module
 	# ScriptsToProcess = @()
-	
+
 	# Type files (.ps1xml) to be loaded when importing this module
 	TypesToProcess = @('xml\WindowsEventForwarding.Types.ps1xml')
-	
+
 	# Format files (.ps1xml) to be loaded when importing this module
 	FormatsToProcess = @('xml\WindowsEventForwarding.Format.ps1xml')
-	
+
 	# Modules to import as nested modules of the module specified in
 	# ModuleToProcess
 	# NestedModules = @()
-	
+
 	# Functions to export from this module
 	FunctionsToExport  = @(
 		'Get-WEFSubscription'
+		'Get-WEFSubscriptionRuntimestatus'
 		'New-WEFCollectorInitiatedSubscription'
 		'New-WEFSourceInitiatedSubscription'
 		'New-WEFSubscription'
@@ -78,44 +79,44 @@ All cmdlets are build with
 		'Resume-WEFSubscription'
 		'Set-WEFSubscription'
 	)
-	
+
 	# Cmdlets to export from this module
 	CmdletsToExport = @()
-	
+
 	# Variables to export from this module
 	VariablesToExport = @()
-	
+
 	# Aliases to export from this module
 	AliasesToExport = @()
-	
+
 	# List of all modules packaged with this module
 	ModuleList = @()
-	
+
 	# List of all files packaged with this module
 	FileList = @()
-	
+
 	# Private data to pass to the module specified in ModuleToProcess. This may also contain a PSData hashtable with additional module metadata used by PowerShell.
 	PrivateData = @{
-		
+
 		#Support for PowerShellGet galleries.
 		PSData = @{
-			
+
 			# Tags applied to this module. These help with module discovery in online galleries.
 			# Tags = @()
-			
+
 			# A URL to the license for this module.
 			# LicenseUri = ''
-			
+
 			# A URL to the main website for this project.
 			# ProjectUri = ''
-			
+
 			# A URL to an icon representing this module.
 			# IconUri = ''
-			
+
 			# ReleaseNotes of this module
 			# ReleaseNotes = ''
-			
+
 		} # End of PSData hashtable
-		
+
 	} # End of PrivateData hashtable
 }
