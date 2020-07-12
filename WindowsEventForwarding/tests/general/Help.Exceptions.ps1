60355307b2d0a6206b68e16f4b27674e4f512654
Andi Bellstedt <bellstedt@gmx.de>
2020-07-12 18:07:10 +0200

    migrate pester test to pester version 5

--- WindowsEventForwarding/tests/general/Help.Exceptions.ps1
+++ WindowsEventForwarding/tests/general/Help.Exceptions.ps1
@@ -1,26 +1,26 @@
-# List of functions that should be ignored
+﻿# List of functions that should be ignored
 $global:FunctionHelpTestExceptions = @(
-    
+
 )
 
 <#
-  List of arrayed enumerations. These need to be treated differently. Add full name.
-  Example:
+List of arrayed enumerations. These need to be treated differently. Add full name.
+Example:
 
-  "Sqlcollaborative.Dbatools.Connection.ManagementConnectionType[]"
+"Sqlcollaborative.Dbatools.Connection.ManagementConnectionType[]"
 #>
 $global:HelpTestEnumeratedArrays = @(
-	
+
 )
 
 <#
-  Some types on parameters just fail their validation no matter what.
-  For those it becomes possible to skip them, by adding them to this hashtable.
-  Add by following this convention: <command name> = @(<list of parameter names>)
-  Example:
+Some types on parameters just fail their validation no matter what.
+For those it becomes possible to skip them, by adding them to this hashtable.
+Add by following this convention: <command name> = @(<list of parameter names>)
+Example:
 
-  "Get-DbaCmObject"       = @("DoNotUse")
+"Get-DbaCmObject"       = @("DoNotUse")
 #>
 $global:HelpTestSkipParameterType = @{
-    
+
 }
